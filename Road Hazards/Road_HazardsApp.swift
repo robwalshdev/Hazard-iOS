3eb679ad53293a4da3a3e968e0486e9644acc63f
Robert Walsh <robwalshdev@gmail.com>
2021-01-26 14:46:58 +0000

    Initial Commit

---
+++ Road Hazards/Road_HazardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Road_HazardsApp.swift
+//  Road Hazards
+//
+//  Created by Robert Walsh on 26/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct Road_HazardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
